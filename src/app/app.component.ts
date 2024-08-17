721bb52326b3e31b1cb3c098f4bacb835c3e99c0
MatheusCFBT <matheuscaldanaf@gmail.com>
2024-08-17 16:01:16 -0300

    (On configurin) Routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { MenuComponent } from './navegacao/menu/menu.component';
 import { HomeComponent } from './navegacao/home/home.component';
 import { FooterComponent } from './navegacao/footer/footer.component';
+import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [MenuComponent, HomeComponent, FooterComponent],
+  imports: [RouterOutlet,MenuComponent, HomeComponent, FooterComponent],
   templateUrl: './app.component.html',
 })
 
